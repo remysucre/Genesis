5199964650e5b688213c86812571c75e720db1c8
Yisu Wang <ywang30@cs.tufts.edu>
2015-08-11 13:29:02 -0400

    parsing

--- experiment/Parser.hs
+++ experiment/Parser.hs
@@ -1,65 +1,82 @@
+import System.Environment
 import Text.ParserCombinators.Parsec.Number
 import Text.ParserCombinators.Parsec
 
 -- A Log file contains 0 or more runs, each bracketed by start and end
 -- 
+logFile :: GenParser Char st [(Int, Int, Int, Float, [([String], String)], String, String)]
 logFile = 
-    do result <- many run
+    do result <- manyTill run eof
        eof
        return result
 
+run :: GenParser Char st (Int, Int, Int, Float, [([String], String)], String, String)
 run =
-    do content <- between start end
-       result  <- runContent content
+    do start
+       result <- runContent
+       end
        return result
 
-start = string ">>>>>>>>>>>>>>FINISH OPTIMIZATION>>>>>>>>>>>>>>>\n"
+end :: GenParser Char st String
+end = string ">>>>>>>>>>>>>>FINISH OPTIMIZATION>>>>>>>>>>>>>>>\n"
 
-end   = string ">>>>>>>>>>>>>>>START OPTIMIZATION>>>>>>>>>>>>>>>\n"
+start :: GenParser Char st String
+start = string ">>>>>>>>>>>>>>>START OPTIMIZATION>>>>>>>>>>>>>>>\n"
 
+runContent :: GenParser Char st (Int, Int, Int, Float, [([String], String)], String, String)
 runContent =
     do p  <- pop
        g  <- maxg
        a  <- arch
        b  <- base
-       gs <- many gen
+       gs <- manyTill gen (try (string ">>>>>>>"))
        w  <- winner
        o  <- out
        return (p, g, a, b, gs, w, o) --TODO 
 
+pop :: GenParser Char st Int
 pop = 
     do n <- int
        s <- string " pop\n"
        return n
+
+maxg :: GenParser Char st Int
 maxg = 
     do n <- int
        s <- string " gens\n"
        return n
 
+arch :: GenParser Char st Int
 arch = 
     do n <- int
        s <- string " arch\n"
        return n
 
-
+base :: GenParser Char st Float
 base = 
     do s <- string "Basetime is: "
        f <- floating
        nl <- newline
        return f
+
+gen :: GenParser Char st ([String], String)
 gen = 
-    do ss <- many strand
+    do ss <- manyTill strand (try (string "best"))
        b  <- best
        return (ss, b)
+
+strand :: GenParser Char st String
 strand = 
     do string "bits "
        ds <- many digit
        newline
        return ds
 
-
+best :: GenParser Char st String
 best = 
     do manyTill anyChar newline -- TODO
+
+winner :: GenParser Char st String
 winner = 
     do string "done evolving!\n"
        string "best entity (GA): "
@@ -67,59 +84,14 @@ winner =
        newline
        return ds
 
-
-
+out :: GenParser Char st String
 out = many anyChar
 
-main = do print $ parse logFile "uk" "2 arch\n"
+main = 
+    do [fp] <- getArgs
+       fc <- readFile fp
+       print $ parse logFile "uk" fc
 
 -- main = do print $ parse gen "(uk)" "bits 1010101001\nbits 1010101001\nasdfasdf\n"
 
 -- parseLog input = parse logFile "(unknown)" input
-
-
-
-
-
-
-
-
-
-
-
-{-
--- Each line contains 1 or more cells, separated by a comma
-line :: GenParser Char st [String]
-line = 
-    do result <- cells
-       eol                       -- end of line
-       return result
-       
--- Build up a list of cells.  Try to parse the first cell, then figure out 
--- what ends the cell.
-cells :: GenParser Char st [String]
-cells = 
-    do first <- cellContent
-       next <- remainingCells
-       return (first : next)
-
--- The cell either ends with a comma, indicating that 1 or more cells follow,
--- or it doesn't, indicating that we're at the end of the cells for this line
-remainingCells :: GenParser Char st [String]
-remainingCells =
-    (char ',' >> cells)            -- Found comma?  More cells coming
-    <|> (return [])                -- No comma?  Return [], no more cells
-
--- Each cell contains 0 or more characters, which must not be a comma or
--- EOL
-cellContent :: GenParser Char st String
-cellContent = 
-    many (noneOf ",\n")
-       
-
--- The end of line character is \n
-eol :: GenParser Char st Char
-eol = char '\n'
-parseCSV :: String -> Either ParseError [[String]]
-parseCSV input = parse csvFile "(unknown)" input
--}
