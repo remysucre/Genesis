982afe9e050e5ac734c09fd0d6f3ae0aca2e3090
remy <remy.sucre@gmail.com>
2015-08-16 15:57:40 -0400

    THREAD

--- leakZoo/thread.hs
+++ leakZoo/thread.hs
@@ -5,16 +5,20 @@ import Control.Monad
 
 main = do
     mv <- newMVar [1..1000]
-    upgraderThread mv
+    upgraderThread mv 100000
 
-upgraderThread :: MVar [Int] -> IO ()
-upgraderThread chanMVar = forever job
+upgraderThread :: MVar [Int] -> Int -> IO ()
+upgraderThread chanMVar 0 = do
+  ns <- readMVar chanMVar
+  print ns
+upgraderThread chanMVar n = do job
     where
         job = do
             vlist <- takeMVar chanMVar
             let reslist = strictList $ map id vlist
-            -- fix: let !reslist = strictList $ map id vlist
+            -- let !reslist = strictList $ map id vlist
             putMVar chanMVar reslist
+            upgraderThread chanMVar (n - 1)
 
         strictList xs = if all p xs then xs else []
             where p x = x `seq` True
