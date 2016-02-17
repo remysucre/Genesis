7fbb75e549306dc0f9a6af5e9286e867bc25f336
Yisu Wang <ywang30@cs.tufts.edu>
2016-02-16 20:56:39 -0500

    AESON

--- gcbench.hs
+++
@@ -1,6 +0,0 @@
-import Profiling
-
-main = do
-    buildProj "gcSimulator"
-    stats <- benchmark "gcSimulator" 1
-    print stats
--- toy/Main.hs
+++
@@ -1,4 +0,0 @@
-{-# LANGUAGE BangPatterns #-}
-module Main (main) where
-me (a, b) = a
-main = print $ me (1, undefined)
