ed44dcbf64b695bd0f713bfe9af8d1169e0e6537
Diogenes Nunez <dan@cs.tufts.edu>
2016-03-04 11:38:13 -0500

    Added comments that explain how to use runGenesis.sh

--- runGenesis.sh
+++ runGenesis.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# How to run:
+#
+# bash runGenesis.sh <path> <pop> <gen> <arch>
+#
+# <path>: path to directory with the files
+# <pop>: Size of population per generation
+# <gens>: Number of generations
+# <arch>: Number of best genes that survive each generation
 ./.stack-work/install/*/lts-2.22/7.8.4/bin/Genesis $1 $2 $3 $4
