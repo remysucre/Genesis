73f074d95a13700e2ba3721e6703bdaebe2670a7
Yisu Wang <ywang30@cs.tufts.edu>
2016-03-13 15:36:16 -0400

    setting up for runtime run

--- gcSimulator/gcmem.sh
+++ gcSimulator/gcmem.sh
@@ -1,8 +1,8 @@
 # cp /data/remy/fop.trace /data/remy/temp.trace &&
-cp nobangs.hs Main.hs && cabal run && mv timing.temp nobangstiming.log &&
-cp bangs.hs Main.hs && cabal run && mv timing.temp bangstiming.log &&
-cp gen.hs Main.hs && cabal run && mv timing.temp gentiming.log &&
-cp gen2.hs Main.hs && cabal run && mv timing.temp gen2timing.log 
+cp nobangs.hs Main.hs && cabal run > /dev/null && mv timing.temp nobangstiming.log &&
+cp bangs.hs Main.hs && cabal run > /dev/null && mv timing.temp bangstiming.log &&
+cp gen.hs Main.hs && cabal run > /dev/null && mv timing.temp gentiming.log &&
+cp gen2.hs Main.hs && cabal run > /dev/null && mv timing.temp gen2timing.log 
 # mkdir fop && mv *timing* fop &&
 
 # cp /data/remy/tradebeans.trace /data/remy/temp.trace &&
