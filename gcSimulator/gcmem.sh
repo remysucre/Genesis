0426ed72e9cd243f7e3c8a01a6b05af08c195ca6
Yisu Wang <ywang30@cs.tufts.edu>
2016-03-14 18:45:12 -0400

    nathan gc

--- gcSimulator/gcmem.sh
+++ gcSimulator/gcmem.sh
@@ -1,9 +1,8 @@
-mv /data/remy/halfbatik.trace /data/remy/temp.trace &&
 cp nobangs.hs Main.hs && cabal run > /dev/null && mv timing.temp nobangstiming.log &&
 cp bangs.hs Main.hs && cabal run > /dev/null && mv timing.temp bangstiming.log &&
 cp gen.hs Main.hs && cabal run > /dev/null && mv timing.temp gentiming.log &&
 # cp gen2.hs Main.hs && cabal run > /dev/null && mv timing.temp gen2timing.log 
-mkdir half && mv *timing* half
+mkdir train && mv *timing* train
 
 # cp /data/remy/tradebeans.trace /data/remy/temp.trace &&
 # cp nobangs.hs Main.hs && cabal run && mv timing.temp nobangstiming.log &&
