89f9859c898f3db66c946b6ab2e2b5a5199f971c
Yisu Wang <ywang30@cs.tufts.edu>
2016-04-02 18:47:09 -0400

    fib finish

--- fib/fibSurvivor.hs
+++ fib/fibSurvivor.hs
@@ -3,5 +3,5 @@ module Main (main) where
  
 fib :: Int -> Integer -> Integer -> Integer
 fib 0 _ b = b
-fib n d c = fib (n - 1) c (d + c)
+fib n d (!c) = fib (n - 1) c (d + c)
 main = print $ fib 300000 0 1
