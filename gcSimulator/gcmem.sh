9627fa9486593df64116abd337140dc76f40bdc1
Yisu Wang <ywang30@cs.tufts.edu>
2016-04-30 14:35:59 -0400

    gc stuff

--- gcSimulator/gcmem.sh
+++ gcSimulator/gcmem.sh
@@ -1,8 +1,16 @@
+cp /data/remy/luindex.traceaa /data/remy/temp.trace &&
 cp nobangs.hs Main.hs && cabal run > /dev/null && mv timing.temp nobangstiming.log &&
 cp bangs.hs Main.hs && cabal run > /dev/null && mv timing.temp bangstiming.log &&
 cp gen.hs Main.hs && cabal run > /dev/null && mv timing.temp gentiming.log &&
 # cp gen2.hs Main.hs && cabal run > /dev/null && mv timing.temp gen2timing.log 
-mkdir train && mv *timing* train
+mkdir lua && mv *timing* lua &&
+
+cp /data/remy/luindex.traceab /data/remy/temp.trace &&
+cp nobangs.hs Main.hs && cabal run > /dev/null && mv timing.temp nobangstiming.log &&
+cp bangs.hs Main.hs && cabal run > /dev/null && mv timing.temp bangstiming.log &&
+cp gen.hs Main.hs && cabal run > /dev/null && mv timing.temp gentiming.log &&
+# cp gen2.hs Main.hs && cabal run > /dev/null && mv timing.temp gen2timing.log 
+mkdir lub && mv *timing* lub
 
 # cp /data/remy/tradebeans.trace /data/remy/temp.trace &&
 # cp nobangs.hs Main.hs && cabal run && mv timing.temp nobangstiming.log &&
