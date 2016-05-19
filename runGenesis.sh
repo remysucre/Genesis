7d2e31a6f19fefdb2ee71e30719aa1fa30af234f
Yisu Wang <ywang30@cs.tufts.edu>
2016-05-18 22:32:43 -0400

    gcrun

--- runGenesis.sh
+++ runGenesis.sh
@@ -8,4 +8,5 @@
 # <pop>: Size of population per generation
 # <gens>: Number of generations
 # <arch>: Number of best genes that survive each generation
-/usr/bin/time -a -o "$1".log ./.stack-work/install/*/lts-2.22/7.8.4/bin/Genesis $1 $2 $3 $4 2>&1 | tee "$1".log 
+/usr/bin/time -a -o "$1".log ./.stack-work/install/*/lts-2.22/7.8.4/bin/Genesis $1 $2 $3 $4 2>&1 | tee "$1".log &&
+date >> "$1".log ; git log --pretty=format:'%h' -n 1 >> "$1".log
